57f867bd85b100fad52263d4423c54ddece929ad
Klezya <benjamin.nunez.beat@gmail.com>
2024-09-10 11:34:08 -0300

    ReDo Solemne1

--- Solemne1/src/app/app.config.ts
+++ Solemne1/src/app/app.config.ts
@@ -1,19 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-import { FormsModule } from '@angular/forms'; // Importa FormsModule
+
 import { routes } from './app.routes';
-import { TodoComponent } from './todo/todo.component'; // Asegúrate de importar el componente
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
-    FormsModule, // Incluye FormsModule aquí
-    TodoComponent
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
--- Solemne1/src/main.ts
+++ Solemne1/src/main.ts
@@ -1,13 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Importa FormsModule aquí
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(FormsModule), // Usa importProvidersFrom para FormsModule
-  ],
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
