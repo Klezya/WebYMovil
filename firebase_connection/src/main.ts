09eb4b41df96003003d876b0683b6e456c322529
KsmBlitz <vjestayvaldivia@gmail.com>
2024-09-11 17:23:37 -0300

    Firebase

---
+++ firebase_connection/src/main.ts
@@ -0,0 +1,12 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ]
+});
